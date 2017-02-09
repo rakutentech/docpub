24ba43bd0d82b381bca5be14170880d8ac4c5e77
Ilia Isupov <swinx89@gmail.com>
2017-02-09 14:35:16 +0900

    Implemented searching zendesk ids by path in basic document

--- test/unit/document.js
+++ test/unit/document.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const Document = require('../../lib/document');
 
 describe('Document', () => {
@@ -48,4 +49,132 @@ describe('Document', () => {
                 });
         });
     });
+
+    describe('setChildren', () => {
+        it('should throw if any of children is not instance of Document', () => {
+            const document = new Document('path');
+
+            expect(() => document.setChildren([{}])).to.throw(/Document/);
+        });
+
+        it('should set children of the document', () => {
+            const document = new Document('path');
+            const child = new Document('another_path');
+
+            child.meta = {zendeskId: 123};
+            document.setChildren([child]);
+
+            expect(document.findByPath('another_path')).to.be.equal(123);
+        });
+
+        it('should remove children if called with falsy argument', () => {
+            const document = new Document('path');
+            const child = new Document('another_path');
+
+            child.meta = {zendeskId: 123};
+            document.setChildren([child]);
+            document.setChildren();
+
+            expect(document.findByPath('another_path')).to.be.undefined;
+        });
+    });
+
+    describe('findByPath', () => {
+        it('should return zendeskId of entity for corresponding path in tree', () => {
+            const document = buildDocumentTree_({id: 123});
+
+            expect(document.findByPath('')).to.be.eql(123);
+        });
+
+        it('should accept path as array of components', () => {
+            const document = buildDocumentTree_({id: 123});
+
+            expect(document.findByPath([])).to.be.eql(123);
+        });
+
+        it('should refer child when path contains string name', () => {
+            const document = buildDocumentTree_({
+                id: 123,
+                children: [{
+                    id: 456,
+                    path: 'bar'
+                }]
+            });
+
+            expect(document.findByPath('bar')).to.be.eql(456);
+        });
+
+        it('should refer to self when path contains `.`', () => {
+            const document = buildDocumentTree_({
+                id: 123
+            });
+
+            expect(document.findByPath('.')).to.be.eql(123);
+        });
+
+        it('should refer to parent when path contains `..`', () => {
+            const document = buildDocumentTree_({
+                id: 123,
+                children: [{
+                    id: 456,
+                    path: 'bar'
+                }]
+            });
+
+            expect(document.findByPath('bar/..')).to.be.eql(123);
+        });
+
+        it('should navigate by complex path with all special components presented', () => {
+            const document = buildDocumentTree_({
+                id: 123,
+                children: [
+                    {
+                        id: 456,
+                        path: 'bar'
+                    },
+                    {
+                        id: 789,
+                        path: 'baz'
+                    }
+                ]
+            });
+            const pathToSearch = './baz/../bar/.././bar';
+
+            expect(document.findByPath(pathToSearch)).to.be.eql(456);
+        });
+
+        it('should ignore multiple `/` delimiters', () => {
+            const document = buildDocumentTree_({id: 123});
+
+            expect(document.findByPath('.//')).to.be.equal(123);
+        });
+
+        it('should return own zendeskId for empty path', () => {
+            const document = buildDocumentTree_({id: 123});
+
+            expect(document.findByPath()).to.be.equal(123);
+        });
+    });
 });
+
+function buildDocumentTree_(scheme, parent) {
+    scheme = _.defaults(scheme || {}, {
+        id: 1,
+        path: 'default_path',
+        children: []
+    });
+
+    const document = documentForScheme_(scheme, parent);
+
+    document.setChildren(scheme.children.map(childScheme => buildDocumentTree_(childScheme, document)));
+
+    return document;
+}
+
+function documentForScheme_(scheme, parent) {
+    const document = new Document(scheme.path, parent);
+
+    document.meta = {zendeskId: scheme.id};
+
+    return document;
+}
